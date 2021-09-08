96d1eb125ff2c9837cb729aa7c5d0ec6ab0588e9
Ping Li <imliping@gmail.com>
2021-09-09 01:16:19 +0800

    some change

--- CryptoCommon/Shared/ExchProxy/IPositionProxy.cs
+++ CryptoCommon/Shared/ExchProxy/IPositionProxy.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PortableCSharpLib.Interace;
+using PortableCSharpLib.Util;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +10,16 @@ using System.Threading.Tasks;
 namespace CryptoCommon.Shared.ExchProxy
 {
     // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
-    public class AccountPostion
+    public class AccountPostion : EqualAndCopyUseReflection<AccountPostion>, IIdEqualCopy<AccountPostion>
     {
+        public string Id => Symbol;
+
+        public string InstType { get; set; }
         public string Symbol { get; set; }
         public string Ccy { get; set; }
-        public string Side { get; set; }
+
         public double Pos { get; set; }
+        public string PosSide { get; set; }
         public double AvailPos { get; set; }
         public double AvgPrice { get; set; }
         public double LiqPrice { get; set; }
@@ -25,11 +31,29 @@ namespace CryptoCommon.Shared.ExchProxy
         public double Mmr { get; set; }
     }
 
+    public class FZPosition: EqualAndCopyUseReflection<FZPosition>, IIdEqualCopy<FZPosition>
+    {
+        public string Id => Symbol;
+        public string Symbol
+        {
+            get
+            {
+                if (Long != null)
+                    return Long.Symbol;
+                else if (Short != null)
+                    return Short.Symbol;
+                return null;
+            }
+        }
+        public AccountPostion Long { get; set; }
+        public AccountPostion Short { get; set; }
+    }
+
     public interface IPositionProxy
     {
-        ConcurrentDictionary<string, AccountPostion> Positions { get; }
-        AccountPostion GetPosition(string symbol);
-        event PortableCSharpLib.EventHandlers.ItemChangedEventHandler<AccountPostion> OnAccountPositionUpdated;
-        event PortableCSharpLib.EventHandlers.ItemChangedEventHandler<List<AccountPostion>> OnAccountPositionListUpdated;
+        ConcurrentDictionary<string, FZPosition> Positions { get; }
+        FZPosition GetPosition(string symbol);
+        event PortableCSharpLib.EventHandlers.ItemChangedEventHandler<FZPosition> OnAccountPositionUpdated;
+        event PortableCSharpLib.EventHandlers.ItemChangedEventHandler<List<FZPosition>> OnAccountPositionListUpdated;
     }
 }

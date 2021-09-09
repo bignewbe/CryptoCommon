855d41a44fd0cdaf280f5825dad272028f2fb8d6
Ping Li <imliping@gmail.com>
2021-09-10 01:41:25 +0800

    some change

---
+++ CryptoCommon/Shared/ExchProxy/AccountBalance.cs
@@ -0,0 +1,28 @@
+﻿using PortableCSharpLib.Interace;
+using PortableCSharpLib.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoCommon.Shared.ExchProxy
+{
+    public class AccountBalance : EqualAndCopyUseReflection<AccountBalance>, IIdEqualCopy<AccountBalance>
+    {
+        public string Id { get { return Currency; } }
+        public double Balance { get { return Available + Hold; } }
+        public string Currency { get; set; }
+        public double Available { get; set; }
+        public double Hold { get; set; }
+
+        public AccountBalance()
+        {
+        }
+
+        public AccountBalance(AccountBalance other)
+        {
+            this.Copy(other);
+        }
+    }
+}
---
+++ CryptoCommon/Shared/ExchProxy/AccountPosition.cs
@@ -0,0 +1,40 @@
+﻿using PortableCSharpLib.Interace;
+using PortableCSharpLib.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoCommon.Shared.ExchProxy
+{
+    public class AccountPosition : EqualAndCopyUseReflection<AccountPosition>, IIdEqualCopy<AccountPosition>
+    {
+        public string Id => Symbol;
+
+        public string InstType { get; set; }
+        public string Symbol { get; set; }
+        public string Ccy { get; set; }
+
+        public double Pos { get; set; }
+        public string PosSide { get; set; }
+        public double AvailPos { get; set; }
+        public double AvgPrice { get; set; }
+        public double LiqPrice { get; set; }
+        public double Interest { get; set; }
+        public long CreatedTime { get; set; }
+        public long UpdatedTime { get; set; }
+        public string MarginMode { get; set; }
+        public double Margin { get; set; }
+        public double Mmr { get; set; }
+
+        public AccountPosition()
+        {
+        }
+
+        public AccountPosition(AccountPosition other)
+        {
+            this.Copy(other);
+        }
+    }
+}
